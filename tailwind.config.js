2b315ec043ca0935f863412209d0056786b7d730
Solo Coder <148307188+ShanMihirangaHerath@users.noreply.github.com>
2024-07-13 08:05:48 +0530

    Initial commit.

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+
+const {fontFamily} = require('tailwindcss/defaultTheme')
+
+
+
+module.exports = {
+  content: [
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: ['var(--font-mont)',...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
+
