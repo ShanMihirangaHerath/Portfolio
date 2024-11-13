e2512aa23750f308c607fad92ba3568f0d08f4cf
Solo Coder <148307188+ShanMihirangaHerath@users.noreply.github.com>
2024-11-14 01:16:51 +0530

    Apply Dark Mode

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,27 +3,29 @@ import { motion, useScroll } from "framer-motion";
 import ListIcon from "./ListIcon";
 
 const Details = ({ position, company, companyLink, time, address, work }) => {
-    const ref = useRef(null);
+  const ref = useRef(null);
   return (
-    <li ref={ref} className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between">
-      
-      <ListIcon reference={ref}/>
+    <li
+      ref={ref}
+      className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between"
+    >
+      <ListIcon reference={ref} />
       <motion.div
-      initial={{y:50}}
-      whileInView={{y:0}}
-      transition={{duration:0.5, type:"spring"}}
+        initial={{ y: 50 }}
+        whileInView={{ y: 0 }}
+        transition={{ duration: 0.5, type: "spring" }}
       >
         <h3 className="capitalize font-bold text-2xl">
           {position}&nbsp;
           <a
             href={companyLink}
             target="_blank"
-            className="text-primary capitalize"
+            className="text-primary dark:text-primaryDark capitalize"
           >
             @{company}
           </a>
         </h3>
-        <span className="capitalize font-medium text-dark/75">
+        <span className="capitalize font-medium text-dark/75 dark:text-light/75">
           {time}&nbsp;|&nbsp;{address}
         </span>
         <p className="font-medium w-full">{work}</p>
@@ -46,7 +48,7 @@ const Experience = () => {
       <div ref={ref} className="w-[75%] mx-auto relative">
         <motion.div
           style={{ scaleY: scrollYProgress }}
-          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top "
+          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light"
         />
 
         <ul className="w-full flex flex-col items-start justify-between ml-4">
@@ -75,8 +77,6 @@ const Experience = () => {
             product recommendations and interactive user reviews, while ensuring 
             optimal performance, scalability, and reliability of the application."
           />
-
-          
         </ul>
       </div>
     </div>
